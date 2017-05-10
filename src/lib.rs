613b2667524a1747b1ceaa65dd62929c25016474
viperscape <chrisfgill@gmail.com>
2017-05-10 16:10:38 -0400

    basic state to exit

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate imgui;
+
 extern crate glium;
 extern crate imgui_glium_renderer;
+
 
 pub mod interface;
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
+#[macro_use]
+extern crate imgui;
 extern crate mush;
 use mush::interface::Interface;
+use imgui::Ui;
 
 const CLEAR_COLOR: (f32, f32, f32, f32) = (0.2, 0.2, 0.2, 1.0);
 
 fn main() {
     let mut ifc = Interface::init();
-
+    let mut state = State::default();
+    
     loop {
-        let r = ifc.render(CLEAR_COLOR, |ui| {} );
-        if !r { break }
+        let r = ifc.render(CLEAR_COLOR, |ui| { menu(ui, &mut state) } );
+        if !r | state.exit{ break }
     }
+}
+
+struct State {
+    exit: bool,
+}
+impl Default for State {
+    fn default() -> Self {
+        State { exit: false }
+    }
+}
+
+
+fn menu (ui: &Ui, state: &mut State) {
+    ui.main_menu_bar(|| {
+        ui.menu(im_str!("File"))
+            .build(|| {
+                ui.menu_item(im_str!("Exit"))
+                    .selected(&mut state.exit)
+                    .build();
+            });
+    });
 }
